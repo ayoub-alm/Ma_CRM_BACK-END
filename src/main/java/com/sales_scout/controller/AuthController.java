b87c027b475c430dfce22cc1d3a2e06808fd0d9e
Ayoub <alayoub33@gmail.com>
2025-07-17 15:34:09 +0100

    bug fixing 3

--- src/main/java/com/sales_scout/controller/AuthController.java
+++ src/main/java/com/sales_scout/controller/AuthController.java
@@ -24,7 +24,7 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/auth")
+@RequestMapping("/api/auth")
 public class AuthController {
 
     private static final Logger logger = LoggerFactory.getLogger(AuthController.class);

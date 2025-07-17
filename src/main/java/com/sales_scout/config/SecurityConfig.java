3d167a9b69ae54907631ba0534beea8715b3a0b9
Ayoub <alayoub33@gmail.com>
2025-07-17 14:00:40 +0100

    bug fixing

--- src/main/java/com/sales_scout/config/SecurityConfig.java
+++ src/main/java/com/sales_scout/config/SecurityConfig.java
@@ -85,6 +85,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.csrf(csrf -> csrf.disable())
+                .cors().and()
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/user").authenticated()
                         .requestMatchers("/home", "/api/leads/dashboard").permitAll()
